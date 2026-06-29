2d30088abfb49d17e8f5be8bde76d14b7fdeb8b3
reid <reid.kaiser@darkwolf.io>
2026-06-29 16:05:52 -0600

    Added pandas for experimentation and data

--- src/vehicle_search/cli.py
+++ src/vehicle_search/cli.py
@@ -1,5 +1,6 @@
 import click
 
+from vehicle_search.file_read import read_vehicle_sales
 from vehicle_search.rest.find import find_vehicle
 
 
@@ -23,5 +24,15 @@ def find(make: str, year: int) -> None:
     click.echo(f"Finding make: {make}")
     find_vehicle(make, year)
 
+@app.command()
+@click.option("--year", help="Sale Year")
+@click.option("--make", help="Make of vehicle (i.e. Honda)")
+@click.option("--model", help="Model of vehicle (i.e. Accord)")
+def find_sales(make: str, model: str, year: int) -> None:
+    """Find car sales"""
+    click.echo(f"Finding sales for make: {make}, model: {model}, year: {year}")
+    read_vehicle_sales(make, model, year)
+
+    
 if __name__ == "__main__":
     app()
---
+++ src/vehicle_search/file_read.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+def read_vehicle_sales(make: str, model: str, sale_year: int):
+
+    vehicle_sales = pd.read_csv("data/car_prices.csv")
+
+    vehicle_sales["saledate_clean"] = (
+        vehicle_sales["saledate"]
+        .str.replace(r"\s+\([A-Z]+\)$", "", regex=True)
+)
+    vehicle_sales["saledate_clean"] = pd.to_datetime(vehicle_sales["saledate_clean"],
+                                               errors="coerce", utc=True)
+    vehicle_sales["sale_year"] = vehicle_sales["saledate_clean"].dt.year
+
+    print(f"{make}, {model}, {sale_year}")
+
+    selected_sales = vehicle_sales[
+        (vehicle_sales["make"].str.strip().str.lower() == make.strip().lower()) &
+        (vehicle_sales["model"].str.strip().str.lower() == model.strip().lower()) &
+        (vehicle_sales["sale_year"] == int(sale_year))
+    ]
+
+    data_frame = selected_sales.head()
+    print(data_frame)
+
+    print(f"Vehicles sold {selected_sales.shape[0]}")
